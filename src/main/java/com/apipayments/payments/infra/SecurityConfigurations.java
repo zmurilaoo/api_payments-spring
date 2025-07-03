870da68a0b52c9a0befe441c771ca8808074e74a
Murilo Braz <159815893+zmurilaoo@users.noreply.github.com>
2025-07-03 11:56:52 -0300

    boraa

--- src/main/java/com/apipayments/payments/infra/SecurityConfigurations.java
+++ src/main/java/com/apipayments/payments/infra/SecurityConfigurations.java
@@ -1,9 +1,24 @@
 package com.apipayments.payments.infra;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-
+@EnableWebSecurity
 public class SecurityConfigurations {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(crsf -> crsf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authrize -> authrize.requestMatchers(HttpMethod.POST, "/payments")
+                        .hasRole("ADMIN").anyRequest().authenticated()).build();
+    }
+
 }
